d0f8ae7113e0e13e155a915f0ee2024813630bc7
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2019-04-14 10:53:20 +0200

    encapsulation: main idea of validation component

--- encapsulation/src/main/java/be/course/design/encapsulation/filecheck/ValidationComponent.java
+++ encapsulation/src/main/java/be/course/design/encapsulation/filecheck/ValidationComponent.java
@@ -1,6 +1,7 @@
 package be.course.design.encapsulation.filecheck;
 
 import java.nio.file.Path;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -20,7 +21,33 @@ import java.util.List;
  * the directory with the translation files
  * the delimiter.
  *
- * the behaviour.
+ * the behaviour, what do i expect this component to do?
+ * This component should read the file to validate line by line and then validate each line. If an
+ * error is found, it should log that error. So basically this component should store errors
+ * as well. We would also want to know on what line that error occurred. Because this component,
+ * at the moment anyway, is reading the file, it should be responsible for the line number as well.
+ *
+ * To me, this looks like this component will be doing a lot of stuff... a lot of tasks tackling
+ * different concerns.... maybe we should start to point out these concerns..
+ *
+ * THOUGHTS ON THE CURRENT DESIGN
+ * --  (i have written down the data and the validateFile method with 2
+ * params: the datafile type and the file to validate) ---
+ * At this time in the process it dawns to me that I have no idea yet how
+ * we will know the datafile type... this is a moment where a wrong impacting design
+ * decision can be made. especially if you want to go fast.
+ *
+ * Also, the validation component needs to know what headerfile and what translation file to use
+ * per type of file. There is a directory with a filename for every type. It dawns to me that I
+ * have no way of letting the validation component know. Here we are at another important design
+ * decision. THe easiest way would be to stuff it in the constructor... big nono!
+ *
+ *
+ * The encapsulation mindset: what do I want this component to do? How can I make sure that interacting
+ * with this component is easy? How can I make the API easy to use?
+ *
+ * So I have written out what I want this component to do, and by that, i have noted down the fields in
+ * this component.
  */
 public class ValidationComponent implements FileValidation {
 
@@ -28,6 +55,7 @@ public class ValidationComponent implements FileValidation {
     private Path headerFiles;
     private Path translationFiles;
     private String delimiter;
+    private List<String> errors = new LinkedList<>();
 
     @Override
     public List<String> validateFile(DataFileType type, Path dataFileToValidate) {
