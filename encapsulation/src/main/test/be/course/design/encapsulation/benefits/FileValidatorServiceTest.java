54102126f37891afc974616fbe06b0873829f829
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2019-11-01 15:13:30 +0100

    another test

--- encapsulation/src/main/test/be/course/design/encapsulation/benefits/FileValidatorServiceTest.java
+++ encapsulation/src/main/test/be/course/design/encapsulation/benefits/FileValidatorServiceTest.java
@@ -1,5 +1,20 @@
+package be.course.design.encapsulation.benefits;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FileValidatorServiceTest {
+
+    @Test
+    public void testFileValidatorService() {
+        /**
+         * This is not a real test, I know. But this is all I need to prove a point :).
+         *
+         * PS: don't do this at home or at work! Tests are very important!
+         */
+
+        assert true;
+    }
 
 }
