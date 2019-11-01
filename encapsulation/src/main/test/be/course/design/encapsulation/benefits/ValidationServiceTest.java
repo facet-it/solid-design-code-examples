4efe1257addbebd825a391b377c940fdb316ac41
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2019-11-01 15:06:28 +0100

    dummy test

--- encapsulation/src/main/test/be/course/design/encapsulation/benefits/ValidationServiceTest.java
+++ encapsulation/src/main/test/be/course/design/encapsulation/benefits/ValidationServiceTest.java
@@ -1,5 +1,18 @@
+package be.course.design.encapsulation.benefits;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ValidationServiceTest {
+
+    @Test
+    public void testValidationService() {
+        /**
+         * I know this is not a real test. I am making a video here, there is no montage, so this is my way of going fast.
+         * But don't do this at home though! Seriously, write your tests!!s
+         */
+        assert true;
+    }
 
 }
