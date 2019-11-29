1ef5b382a32e1e0e2e17fa2949246a066a4a1c7b
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2019-11-29 15:44:23 +0100

    fixed typo

--- encapsulation/src/main/test/be/course/design/encapsulation/benefits/ValidationServiceTest.java
+++ encapsulation/src/main/test/be/course/design/encapsulation/benefits/ValidationServiceTest.java
@@ -2,15 +2,13 @@ package be.course.design.encapsulation.benefits;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ValidationServiceTest {
 
     @Test
     public void testValidationService() {
         /**
          * I know this is not a real test. I am making a video here, there is no montage, so this is my way of going fast.
-         * But don't do this at home though! Seriously, write your tests!!s
+         * But don't do this at home though! Seriously, write your tests!!
          */
         assert true;
     }
